ffa7550897a64c37f3b0f8787c67a90c1844958d
Simon MAGDELAINE <smagdela@student.42.fr>
2021-12-14 14:01:43 +0100

    Implementing pointer drawing (like a pen).

--- sources/fractol.c
+++ sources/fractol.c
@@ -6,7 +6,7 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 16:27:37 by smagdela          #+#    #+#             */
-/*   Updated: 2021/12/14 12:44:30 by smagdela         ###   ########.fr       */
+/*   Updated: 2021/12/14 14:00:43 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ static int	init_events(t_display *display, t_image *image)
 {
 	mlx_loop_hook(display->mlx_ptr, &loop_handler, image);
 	mlx_hook(display->win_ptr, KeyPress, KeyPressMask, &keys_handler, image);
-	mlx_hook(display->win_ptr, KeyRelease, KeyReleaseMask, &keys_rev_handler, image);
+	mlx_hook(display->win_ptr, KeyRelease, KeyReleaseMask,
+		&keys_rev_handler, image);
 	mlx_hook(display->win_ptr, DestroyNotify, StructureNotifyMask,
 		&red_cross_handler, image);
-	mlx_hook(display->win_ptr, MotionNotify, PointerMotionMask,
-		&pointer_handler, image);
-	mlx_hook(display->win_ptr, MotionNotify, ButtonMotionMask, &mouse_handler, image);
-	mlx_hook(display->win_ptr, ButtonRelease, ButtonReleaseMask, &mouse_rev_handler, image);
+	mlx_hook(display->win_ptr, MotionNotify, ButtonMotionMask,
+		&mouse_handler, image);
+	mlx_hook(display->win_ptr, ButtonRelease, ButtonReleaseMask,
+		&mouse_rev_handler, image);
+//	mlx_hook(display->win_ptr, MotionNotify, PointerMotionMask,
+//		&pointer_handler, image);
 //	mlx_mouse_hook(display->win_ptr, &mouse_handler, image);
 	return (0);
 }
--- sources/ft_events.c
+++ sources/ft_events.c
@@ -6,7 +6,7 @@
 /*   By: smagdela <smagdela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:45:38 by smagdela          #+#    #+#             */
-/*   Updated: 2021/12/14 12:42:53 by smagdela         ###   ########.fr       */
+/*   Updated: 2021/12/14 14:00:03 by smagdela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ int	keys_handler(int key_sym, t_image *image)
 	}
 	else if (key_sym == XK_q)
 		draw_circle(image, build_circle(WIN_WIDTH / 3, WIN_HEIGHT / 3,
-			350, 0xff0000));
+				350, 0xff0000));
 	else if (key_sym == XK_w)
 		draw_circle(image, build_circle(WIN_WIDTH * 2 / 3, WIN_HEIGHT / 3,
-			350, 0x00ff00));
+				350, 0x00ff00));
 	else if (key_sym == XK_e)
 		draw_circle(image, build_circle(WIN_WIDTH / 2, WIN_HEIGHT * 2 / 3,
-			350, 0x0000ff));
+				350, 0x0000ff));
 	ft_putchar_fd((char)key_sym, 1);
 	mlx_put_image_to_window(image->display->mlx_ptr,
 		image->display->win_ptr, image->image_ptr, 0, 0);
@@ -43,18 +43,18 @@ int	keys_handler(int key_sym, t_image *image)
 
 int	keys_rev_handler(int key_sym, t_image *image)
 {
-    int color;
+	int	color;
 
-    color = 0;
-    if (key_sym == XK_q)
+	color = 0;
+	if (key_sym == XK_q)
 		draw_circle(image, build_circle(WIN_WIDTH / 3, WIN_HEIGHT / 3,
-			350, color));
+				350, color));
 	else if (key_sym == XK_w)
 		draw_circle(image, build_circle(WIN_WIDTH * 2 / 3, WIN_HEIGHT / 3,
-			350, color));
+				350, color));
 	else if (key_sym == XK_e)
 		draw_circle(image, build_circle(WIN_WIDTH / 2, WIN_HEIGHT * 2 / 3,
-			350, color));
+				350, color));
 	mlx_put_image_to_window(image->display->mlx_ptr,
 		image->display->win_ptr, image->image_ptr, 0, 0);
 	return (0);
@@ -62,6 +62,14 @@ int	keys_rev_handler(int key_sym, t_image *image)
 
 int	mouse_handler(int button, int x, int y, t_image *image)
 {
+	if (x >= 0 && x <= WIN_WIDTH && y >= 0 && y <= WIN_HEIGHT)
+	{
+		ft_putstr_fd("Mouse coordinates = (", 1);
+		ft_putnbr_fd(x, 1);
+		ft_putstr_fd(", ", 1);
+		ft_putnbr_fd(y, 1);
+		ft_putstr_fd(")\n", 1);
+	}
 	if (button == 1)
 	{
 		draw_circle(image, build_circle(x, y, 50, 0xffffff));
@@ -78,6 +86,7 @@ Button value:
 	molette /\ = 4
 	molette \/ = 5
 */
+
 int	mouse_rev_handler(int button, int x, int y, t_image *image)
 {
 	(void)x;
